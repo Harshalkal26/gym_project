045fe594439bf00238f307679e8251996e16dfcc
Harshal Kalgaonkar <harshal.kal25@gmail.com>
2023-04-01 13:03:19 +0530

    Cards alignment changed

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,77 +1,12 @@
-// import React, { useContext } from 'react';
-// import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
-// import { Box, Typography } from '@mui/material';
-// import BodyPart from './BodyPart';
-// import RightArrowIcon from '../assets/icons/right-arrow.png';
-// import LeftArrowIcon from '../assets/icons/left-arrow.png';
-
-// const LeftArrow = () => {
-//   const { scrollPrev } = useContext(VisibilityContext);
-
-//   return (
-//     <Typography onClick={() => scrollPrev()} className="right-arrow">
-//       <img src={LeftArrowIcon} alt="right-arrow" />
-//     </Typography>
-//   );
-// };
-
-// const RightArrow = () => {
-//   const { scrollNext } = useContext(VisibilityContext);
-
-//   return (
-//     <Typography onClick={() => scrollNext()} className="left-arrow">
-//       <img src={RightArrowIcon} alt="right-arrow" />
-//     </Typography>
-//   );
-// };
-
-// const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
-//   <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-//     {data.map((item) => (
-//       <Box
-//         key={item.id || item}
-//         itemId={item.id || item}
-//         title={item.id || item}
-//         m="0 40px"
-//       >
-//         <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} />
-//       </Box>
-//     ))}
-//   </ScrollMenu>
-// );
-
-// export default HorizontalScrollbar;
-import React, { useContext } from 'react';
-import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
-import { Box, Typography } from '@mui/material';
+import React from 'react';
+import { Box } from '@mui/material';
 
 import ExerciseCard from './ExerciseCard';
 import BodyPart from './BodyPart';
-import RightArrowIcon from '../assets/icons/right-arrow.png';
-import LeftArrowIcon from '../assets/icons/left-arrow.png';
 
-const LeftArrow = () => {
-  const { scrollPrev } = useContext(VisibilityContext);
-
-  return (
-    <Typography onClick={() => scrollPrev()} className="right-arrow">
-      <img src={LeftArrowIcon} alt="right-arrow" />
-    </Typography>
-  );
-};
-
-const RightArrow = () => {
-  const { scrollNext } = useContext(VisibilityContext);
-
-  return (
-    <Typography onClick={() => scrollNext()} className="left-arrow">
-      <img src={RightArrowIcon} alt="right-arrow" />
-    </Typography>
-  );
-};
 
 const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
-  <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+  <div>
     {data.map((item) => (
       <Box
         key={item.id || item}
@@ -82,7 +17,7 @@ const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
         {bodyParts ? <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> : <ExerciseCard exercise={item} /> }
       </Box>
     ))}
-  </ScrollMenu>
+  </div>
 );
 
 export default HorizontalScrollbar;
